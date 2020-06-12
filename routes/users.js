d43105e9284dbd92015a973c84d0e4e427240d05
ultrawide <colinchan02@gmail.com>
2020-06-12 16:49:37 -0700

    Add users routing

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', (req, res) => {
+  return res.send('GET HTTP method on user resource');
+});
+ 
+router.post('/', (req, res) => {
+  return res.send('POST HTTP method on user resource');
+});
+ 
+router.put('/', (req, res) => {
+  return res.send('PUT HTTP method on user resource');
+});
+ 
+router.delete('/', (req, res) => {
+  return res.send('DELETE HTTP method on user resource');
 });
 
 module.exports = router;
